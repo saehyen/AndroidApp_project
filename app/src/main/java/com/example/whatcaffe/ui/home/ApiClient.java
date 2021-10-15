ef27efe089a4727822436e36f17234e0436b189e
KRdoncc <cow1730@naver.com>
2021-10-15 16:29:48 +0900

    맵 마커찍기 1단계

---
+++ app/src/main/java/com/example/whatcaffe/ui/home/ApiClient.java
@@ -0,0 +1,30 @@
+package com.example.whatcaffe.ui.home;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient
+{
+    private static final String BASE_URL = "https://dapi.kakao.com/";
+    private static Retrofit retrofit;
+
+    public static Retrofit getApiClient()
+    {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        if (retrofit == null)
+        {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/example/whatcaffe/ui/home/HomeViewModel.java
@@ -0,0 +1,78 @@
+package com.example.whatcaffe.ui.home;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+    private MutableLiveData<List<Place>> mutableCaffePlaceList;
+    private List<Place> caffePlace = null;
+
+
+    private final static String SEARCH_KEYWORD = "대구 신서동 커피";
+    private final static String API_KEY = "KakaoAK 341a477e04a5b89dcdb54cdc2bfc7c47";
+    private final static String LOG_TAG = "Kakao Map App";
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+    public LiveData<List<Place>> getPlaces() {
+        if (mutableCaffePlaceList == null) {
+            mutableCaffePlaceList = new MutableLiveData<List<Place>>();
+            loadPlace();
+        }
+
+        return mutableCaffePlaceList;
+    }
+
+    public void getCaffeListData() {
+        KakaoAPIInterface spotInterface =  ApiClient.getApiClient().create(KakaoAPIInterface.class);
+        Call<CaffeListData> call = spotInterface.getSearchKeyword(API_KEY, SEARCH_KEYWORD);
+
+        call.enqueue(new Callback<CaffeListData>()
+        {
+            //연결 성공 시에 싱행되는 부분
+            @Override
+            public void onResponse(@NonNull Call<CaffeListData> call, @NonNull Response<CaffeListData> response)
+            {
+                Log.e("onSuccess", String.valueOf(response.raw()));
+                caffePlace = response.body().documents;
+                loadPlace();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<CaffeListData> call, @NonNull Throwable t)
+            {
+                Log.e(LOG_TAG, "Request Fail : " + t.getMessage());
+            }
+        });
+    }
+
+    private void loadPlace() {
+        if (caffePlace == null) {
+            return;
+        }
+
+        mutableCaffePlaceList.setValue(caffePlace);
+    }
+}
